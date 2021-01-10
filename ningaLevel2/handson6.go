73ca71ee192afedef9fb0a372b82e642330c1eb9
Nandhakumar Kittusamy <nandhacse94@gmail.com>
2021-01-11 00:06:36 +0530

    Ninja2: handson6

---
+++ ningaLevel2/handson6.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	year1 = iota + 2016;
+	year2 = iota + 2016;
+	year3 = iota + 2016;
+	year4 = iota + 2016;
+)
+func main() {
+
+	fmt.Println(year1);
+	fmt.Println(year2);
+	fmt.Println(year3);
+	fmt.Println(year4);
+}
+
