529e6fa0987ba1f1ef103f0a81fd72bd187250fd
Nandhakumar Kittusamy <nandhacse94@gmail.com>
2021-01-11 00:01:25 +0530

    Ninja2: handson5

---
+++ ningaLevel2/handson5.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//create a RAW STRING LITERAL and assign it to a variable.
+	
+	a := ` hello this is nandha,
+	from "wipro limited";
+	I'm gonna move to , new company
+	called "SCIENTIFIC GAMES"
+	as c++ dev. 
+	hey congrats! `
+	
+	fmt.Println(a);
+}
+
