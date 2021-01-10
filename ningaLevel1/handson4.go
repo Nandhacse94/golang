93adb75ccf34629941bb925da83c1e126f175b13
Nandhakumar Kittusamy <nandhacse94@gmail.com>
2021-01-10 09:15:13 +0530

    Ninja1: handson4

---
+++ ningaLevel1/handson4.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+type numbers int
+var x numbers;
+
+func main() {
+	fmt.Println(x);
+	fmt.Printf("%T\n", x);
+
+	x = 42;
+	
+	fmt.Println(x);
+	fmt.Printf("%T\n", x);
+
+}
+
